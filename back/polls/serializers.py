30421a28a7347a3b222ad962801350fd3206fb31
mehdigoom <medhi.gay@sfr.fr>
2019-10-11 14:13:47 +0200

    back ok

--- back/polls/serializers.py
+++ back/polls/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers, fields
 from . import models
 
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Choice
+        fields = ('choice_text', 'votes')
+
 class QuestionSerializer(serializers.ModelSerializer):
+
+    choices = ChoiceSerializer(source='choice_set', many=True)
+
     class Meta:
         model = models.Question
-        fields = ('question_text', 'pub_date',)
+        fields = ('question_text', 'pub_date', 'choices')
